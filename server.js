3b58d8fac863f1ebe96be131d0b10752f971821b
unknown <rubikonspace@ukr.net>
2023-12-19 21:15:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
+// const mong =
+//   "mongodb+srv://qwe:r1T6QXqsoBOdvv3j@cluster0.tsxivj7.mongodb.net/db-contacts?";
+
 const mong =
-  "mongodb+srv://qwe:r1T6QXqsoBOdvv3j@cluster0.tsxivj7.mongodb.net/db-contacts?";
+  "mongodb+srv://user-test:xzHmucx8gbL7wmC7@cluster0.tkmkwho.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose
   .connect(mong)
