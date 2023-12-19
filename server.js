5d07bb669734e48cc67131c3afd8f7379bcf6dbc
unknown <rubikonspace@ukr.net>
2023-12-19 21:48:33 +0200

    3

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import app from "./app.js";
+import { mong } from "./config.js";
 
 // const mong =
 //   "mongodb+srv://qwe:r1T6QXqsoBOdvv3j@cluster0.tsxivj7.mongodb.net/db-contacts?";
 
-const mong =
-  "mongodb+srv://user-test:xzHmucx8gbL7wmC7@cluster0.tkmkwho.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// const mong =
+//   "mongodb+srv://user-test:xzHmucx8gbL7wmC7@cluster0.tkmkwho.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose
   .connect(mong)
